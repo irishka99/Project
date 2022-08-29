0c64e877a2b88f5e5c7491895b7785b5f970aaa9
LAPTOP-B0S0HB76\Иришка <irishka-kim1@mail.ru>
2022-08-29 22:25:03 +0300

    Добавлена домашка к семинару 7

--- Program.cs
+++ Program.cs
@@ -456,3 +456,145 @@
 // double y = k1 * x + b1;
 // Console.WriteLine($"Точка пересечения двух прямых : {Math.Round(x,2)},{Math.Round(y,2)}");
 
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// double[,] GetArray(int m, int n)
+// {
+//     double[,] matrix = new double[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = Math.Round((new Random().NextDouble() * 10 * new Random().Next(-1, 2)), 1);
+//         }
+//     }
+//     return matrix;
+// }
+// double[,] resultMatrix = GetArray(rows, columns);
+// PrintArray(resultMatrix);
+// void PrintArray(double[,] inputMatrix)
+// {
+//     for (int i = 0; i < inputMatrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputMatrix.GetLength(1); j++)
+//         {
+//             Console.Write(inputMatrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет
+
+// Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// int[,] GetArray(int m, int n)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(11);
+//         }
+//     }
+//     return matrix;
+// }
+// int[,] resultMatrix = GetArray(rows, columns);
+// PrintArray(resultMatrix);
+// void PrintArray(int[,] inputMatrix)
+// {
+//     for (int i = 0; i < inputMatrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputMatrix.GetLength(1); j++)
+//         {
+//             Console.Write(inputMatrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// Console.Write("Введите строку искомого элемента: ");
+// int searchIndexi = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите столбец искомого элемента: ");
+// int searchIndexj = Convert.ToInt32(Console.ReadLine());
+// if (searchIndexi < 0 || searchIndexi > rows - 1 || searchIndexj < 0 || searchIndexj > columns - 1)
+// {
+//     Console.WriteLine("Такого элемента нет.");
+// }
+// else
+// {
+//     Console.WriteLine($"Искомый элемент: {resultMatrix[searchIndexi, searchIndexj]}");
+// }
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+// Console.Write("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// int[] sum = new int[columns];
+// double[] avg = new double[columns];
+
+// int[,] GetArray(int m, int n)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(11);
+//         }
+//     }
+
+//     return matrix;
+// }
+
+// int[,] resultMatrix = GetArray(rows, columns);
+// PrintArrayAndGetSum(resultMatrix);
+// void PrintArrayAndGetSum(int[,] inputMatrix)
+// {
+//     for (int i = 0; i < inputMatrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inputMatrix.GetLength(1); j++)
+//         {
+//             Console.Write(inputMatrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// for (int j = 0; j < resultMatrix.GetLength(1); j++)
+// {
+//     for (int i = 0; i < resultMatrix.GetLength(0); i++)
+//     {
+//         sum[j] += resultMatrix[i, j];
+//     }
+// }
+// for (int i = 0; i < columns; i++)
+// {
+//     avg[i] = Math.Round(Convert.ToDouble(sum[i]) / Convert.ToDouble(rows), 1);
+// }
+// Console.Write($"Среднее арифметическое в каждом столбце:\n[ {String.Join("; ", avg)} ]");
