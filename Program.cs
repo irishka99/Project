0dda377563d80ce6a47da1d4e6a04243b5f109b9
LAPTOP-B0S0HB76\Иришка <irishka-kim1@mail.ru>
2022-09-05 00:06:55 +0300

    добавлена домашка к семинару 9

--- Program.cs
+++ Program.cs
@@ -927,10 +927,83 @@
 
 // PrintArray(Array);
 
+// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+// Console.WriteLine("Введите первое значение (M)");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите последнее значение (N)");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int List(int start, int finish)
+// {
+//     int result = start;
+//     Console.Write($"{result}\t");
+//     if (result < finish)
+//     {
+//         start++;
+//         return List(start, finish);
+//     }
+//     return result;
+// }
+// System.Console.WriteLine("\nГенерируем натуральные числа от M до N:");
+// List(m, n);
 
 
 
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Введите первое значение (M)");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите последнее значение (N)");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
+
+// int Sum(int start, int finish)
+// {
+//     result = start + result;
+//     Console.Write($"{result}\t");
+//     if (start < finish)
+//     {
+//         start++;
+//         return Sum(start, finish);
+//     }
+//     Console.WriteLine($"\nОтвет:{result}");
+//     return result;
+// }
+
+// Console.WriteLine("\nГенерируем сумму натуральных чисел от M до N:");
+// Sum(m, n);
 
 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
+// m = 2, n = 3 -> A(m,n) = 9
 
+// Console.WriteLine("Введите первое неотрицательное число (M)");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе неотрицательное число (N)");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// int Ackerman(int m,int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     if (m > 0 && n == 0)
+//     {
+//         return Ackerman(m - 1, 1);
+//     }
+//     if (m > 0 && n > 0)
+//     {
+//         return Ackerman(m - 1, Ackerman(m, n - 1));
+//     }
+//     return 0;
+// }
+// Console.WriteLine("Вычисляем результат по формуле Аккермана : ");
+// Console.WriteLine(Ackerman(firstNumber, secondNumber));
